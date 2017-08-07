45f59b5c8052e2f9856dc28dc661a74fc042d3e8
MasterWu77 <MasterWu77@gmail.com>
2017-08-07 17:41:39 -0400

    asdfasdf

--- GitHub Tutorial/Form1.cs
+++ GitHub Tutorial/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHub_Tutorial
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
     }
 }
